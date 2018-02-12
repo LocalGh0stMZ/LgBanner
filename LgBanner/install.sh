69f80cc9d4e5cc1183c4d441b4e6d529a229f61c
LocalGh0st <36140262+LocalGh0stMZ@users.noreply.github.com>
2018-02-12 07:38:46 -0600

    arquivos do banner
    este banner foi criado por @localgh0st

---
+++ LgBanner/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+#
+#  ╔╦╗╔═╗╦  ╔═╗╔═╗╦═╗╔═╗╔╦╗
+#   ║ ║╣ ║  ║╣ ║ ╦╠╦╝╠═╣║║║
+#   ╩ ╚═╝╩═╝╚═╝╚═╝╩╚═╩ ╩╩ ╩
+#   Canal:  @AnonymousMz
+#   Grupo:  @NHAnonymousMz
+#   Acervo: @AnonyMz_Bot
+#
+#   ╔╗ ╦ ╦
+#   ╠╩╗╚╦╝
+#   ╚═╝ ╩
+#   ╦  ╔═╗╔═╗╔═╗╦  ╔═╗╦ ╦╔═╗╔═╗╔╦╗
+#   ║  ║ ║║  ╠═╣║  ║ ╦╠═╣║ ║╚═╗ ║
+#   ╩═╝╚═╝╚═╝╩ ╩╩═╝╚═╝╩ ╩╚═╝╚═╝ ╩
+#
+#    LocalGhost :  @LocalGh0st
+#
+################################################
+apt update
+apt upgrade -y
+# Open Folder
+cd $HOME
+cd LgBanner
+# Edit Bash.bashrc File
+cat "logo.txt" >> /usr/bin/bash.bashrc
